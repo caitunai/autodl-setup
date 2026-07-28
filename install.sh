98e57e17fcf4f8eb7626c226f96e8c6da6101cf1
iflamed <iflamed@gmail.com>
2026-07-28 17:56:48 +0800

    fix sudo issue

--- install.sh
+++ install.sh
@@ -40,13 +40,13 @@ fi
 apt-get update
 apt-get -y install pkg-config libopus-dev libopusfile-dev ffmpeg gnupg2 ca-certificates lsb-release ubuntu-keyring
 curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
-    | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+    | tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
 gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
 https://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
-    | sudo tee /etc/apt/sources.list.d/nginx.list
+    | tee /etc/apt/sources.list.d/nginx.list
 echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" \
-    | sudo tee /etc/apt/preferences.d/99nginx
+    | tee /etc/apt/preferences.d/99nginx
 apt-get update
 apt-get -y install nginx
 apt-get clean
