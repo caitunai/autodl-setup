a461dd54be5f35ee8af3001616a5d43e4e582e91
iflamed <iflamed@gmail.com>
2026-07-28 16:39:04 +0800

    fix install bug

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
-cp ./.vllmrc ~/.vllmrc
-echo ". $HOME/.venv/bin/activate" >> ~/.bashrc
-echo ". $HOME/.vllmrc" >> ~/.bashrc
-
 . $HOME/.bashrc
 uv venv
+echo ". $HOME/.venv/bin/activate" >> ~/.bashrc
+
+cp ./.vllmrc ~/.vllmrc
+echo ". $HOME/.vllmrc" >> ~/.bashrc
 . $HOME/.bashrc
 
 apt-get update
