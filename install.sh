6749f1e6b24ca191e0ce58be293e2eb3bdf16446
iflamed <iflamed@gmail.com>
2026-07-28 16:25:26 +0800

    fix bash env bug

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
-uv venv
+cp ./.vllmrc ~/.vllmrc
 echo ". $HOME/.venv/bin/activate" >> ~/.bashrc
+echo ". $HOME/.vllmrc" >> ~/.bashrc
+
+. $HOME/.bashrc
+uv venv
+apt-get update
+apt-get -y install pkg-config libopus-dev libopusfile-dev ffmpeg
